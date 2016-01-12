f2d5e2a8b0ba10f80f343c423ebfb6ad85fa43c7
jon sweeney and megan fayer <mf>
2016-01-12 11:38:12 -0800

    Create uppercase page.

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,16 +1,14 @@
 $(function() {
   $('#blanks form').submit(function(event) {
-    var nameInput = $("input#name").val();
-    var prizeInput = $("input#prize").val();
-    var amountInput = $("input#amount").val();
-    var locationInput = $("input#location").val();
 
-    $('.name').text(nameInput);
-    $('.prize').text(prizeInput);
-    $('.amount').text(amountInput);
-    $('.location').text(locationInput);
 
-    $('#letter').show();
+    var inputInput = $("input#input").val();
+
+    $('.input').text(inputInput.toUpperCase());
+     //add toupper
+
+    $('#response').show();
+
 
     event.preventDefault();
   });
